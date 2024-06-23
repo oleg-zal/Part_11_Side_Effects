a5f115337c3fb76b0c53afccec419c7d5c111181
Oleg Zalozhenkov <oleg.zalozhenkov@gmail.com>
2024-06-23 20:37:15 +0200

    182_183_184 Understanding Effect Dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,13 @@ import DeleteConfirmation from './components/DeleteConfirmation.jsx';
 import logoImg from './assets/logo.png';
 import {sortPlacesByDistance} from "./loc.js";
 
-function App() {
-
-  const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
-  const storedPlaces = storedIds.map((id) =>
+const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+const storedPlaces = storedIds.map((id) =>
     AVAILABLE_PLACES.find((place) => place.id === id)
-  )
-  console.log(storedPlaces)
-  const modal = useRef();
+)
+function App() {
   const selectedPlace = useRef();
+  const [modalIsOpen, setModalIsOpen] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([])
   const [pickedPlaces, setPickedPlaces] = useState(storedPlaces);
 
@@ -31,12 +29,12 @@ function App() {
   }, []);
 
   function handleStartRemovePlace(id) {
-    modal.current.open();
+    setModalIsOpen(true);
     selectedPlace.current = id;
   }
 
   function handleStopRemovePlace() {
-    modal.current.close();
+    setModalIsOpen(false);
   }
 
   function handleSelectPlace(id) {
@@ -59,7 +57,7 @@ function App() {
     setPickedPlaces((prevPickedPlaces) =>
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
-    modal.current.close();
+    setModalIsOpen(false)
 
     const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
     localStorage.setItem(
@@ -70,7 +68,7 @@ function App() {
 
   return (
     <>
-      <Modal ref={modal}>
+      <Modal open={modalIsOpen}>
         <DeleteConfirmation
           onCancel={handleStopRemovePlace}
           onConfirm={handleRemovePlace}
